29ea2f2211c722e04ce48a12843d777d0414f5c8
ClaudiusMinimus <claudiusminimus@gmail.com>
2021-12-16 15:49:20 -0700

    kubejs files

---
+++ .minecraft/kubejs/data/potato/kubejs/potato.js
@@ -0,0 +1,32 @@
+// Enable recipe logging, off by default
+// settings.logAddedRecipes = true
+// settings.logRemovedRecipes = true
+
+// Listen to server recipe event
+onEvent('recipes', event => {
+//events.listen('recipes', function (event) {
+
+    // Add shaped recipe for chests
+    event.shaped('4x minecraft:chest', [
+        'WWW',
+        'W W',
+        'WWW'
+    ], {
+        W: '#minecraft:logs'
+    })
+
+    // Add shapeless recipe for Create Potato book
+    //event.shapeless(item.of('patchouli:guide_book', { 'patchouli:book': 'patchouli:potato' }), ['minecraft:book', 'minecraft:dirt'])
+
+    // Add shapeless recipe for making sticks from logs
+    event.shapeless('16x minecraft:stick', [
+        '#minecraft:logs', '#minecraft:logs'
+    ])
+
+    // Add shapeless recipe for oak slab to oak planks
+    event.shapeless('minecraft:oak_planks', ['minecraft:oak_slab', 'minecraft:oak_slab'])
+
+    // Add shapeless recipe for cobblestone slabs to cobblestone
+    event.shapeless('minecraft:cobblestone', ['minecraft:cobblestone_slab', 'minecraft:cobblestone_slab'])    
+
+})
