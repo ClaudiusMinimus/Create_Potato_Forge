806a7dbf4b790bb515fe3b6e6533362d9a52948d
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-01-09 14:00:05 -0700

    Update script.js

--- .minecraft/kubejs/server_scripts/script.js
+++ .minecraft/kubejs/server_scripts/script.js
@@ -1,37 +1,18 @@
-// priority: 0
 
-console.info('Hello, World! (You will see this line every time server resources reload)')
 
-ServerEvents.recipes(event => {
-    // Add shaped recipe for chests
-    event.shaped('4x minecraft:chest', [
-        'WWW',
-        'W W',
-        'WWW'
-    ], {
-        W: '#minecraft:logs'
-    })
+// Listen to player login event
+PlayerEvents.loggedIn( event => {
+	
+	// Check if player doesn't have "starting_items" stage yet
+	if (!event.player.stages.has('starting_items')) {
+	  // Add the stage
+	  event.player.stages.add('starting_items')
+	  // Give some items to player
+	  event.player.give('minecraft:stone_sword')
+	  event.player.give('minecraft:stone_pickaxe')
+	  event.player.give('minecraft:stone_axe')
+	  event.player.give(Item.of('patchouli:guide_book', '{"patchouli:book":"patchouli:potato"}'))
+	  event.player.give('30x minecraft:apple')
+	}
 
-    // Add shapeless recipe for Create Potato book
-    event.shapeless(Item.of('patchouli:guide_book', '{"patchouli:book":"patchouli:potato"}'), ['minecraft:book', 'minecraft:potato'])
-
-    // Add shapeless recipe for making sticks from logs
-    event.shapeless('16x minecraft:stick', [
-        '#minecraft:logs', '#minecraft:logs'
-    ])
-
-    // Add shapeless recipe for oak slab to oak planks
-    event.shapeless('minecraft:oak_planks', ['minecraft:oak_slab', 'minecraft:oak_slab'])
-
-    // Add shapeless recipe for cobblestone slabs to cobblestone
-    event.shapeless('minecraft:cobblestone', ['minecraft:cobblestone_slab', 'minecraft:cobblestone_slab'])    
-
-})
-
-ServerEvents.tags('item', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-})
+  }) 
