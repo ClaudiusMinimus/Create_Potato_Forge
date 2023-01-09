25cea070e9ccf370422079cf72aa3c30e1455eb8
ClaudiusMinimus <claudiusminimus@gmail.com>
2023-01-09 13:58:17 -0700

    renamed or deleted files

--- .minecraft/kubejs/server_scripts_broken/mixing_netherite.js
+++
@@ -1,7 +0,0 @@
-// Recipe for doubling Ancient Debris to Netherite Scrap
-ServerEvents.recipes('recipes', event => {
-    event.recipes.create.mixing(Item.of('2x minecraft:netherite_scrap'), [
-        'minecraft:ancient_debris'
-    ]).superheated()
-  })
-  
